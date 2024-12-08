e289be86e331f718feea5f31527e0830385df9d6
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-12-08 13:48:33 +0100

    Add files via upload

--- drzewaRozpinajace/drzewaRozpinajace/Graph.cs
+++ drzewaRozpinajace/drzewaRozpinajace/Graph.cs
@@ -21,14 +21,59 @@ namespace drzewaRozpinajace
             StringBuilder sb = new StringBuilder();
             foreach (Edge edge in edges)
             {
-                sb.Append(new string("("+edge.start.data.ToString() + ", " + edge.end.data.ToString() + ") " + edge.weight.ToString()+Environment.NewLine));
+                sb.Append(new string("(" + edge.start.data.ToString() + ", " + edge.end.data.ToString() + ") " + edge.weight.ToString() + Environment.NewLine));
             }
             return sb.ToString();
         }
+        public void Add(Edge k)
+        {
+            if (!this.edges.Contains(k))
+            {
+                this.edges.Add(k);
+            }
+            if (!this.nodes.Contains(k.start))
+            {
+                this.nodes.Add(k.start);
+            }
+            if (!this.nodes.Contains(k.end))
+            {
+                this.nodes.Add(k.end);
+            }
+        }
+        public int ileNowychWezlow(Edge k)
+        {
+            int wynik = 0;
+            if (!this.nodes.Contains(k.start))
+            {
+                wynik++;
+            }
+            if (!this.nodes.Contains(k.end))
+            {
+                wynik++;
+            }
+            return wynik;
+        }
+        void Graf(Edge k)
+        {
+            this.Add(k);
+        }
+
+        void Join(Graph g)
+        {
+            foreach(var k in g.edges)
+            {
+                this.Add(k);
+            }
+        }
+
         public Graph(List<NodeG> nodes, List<Edge> edges)
         {
             this.nodes = nodes;
             this.edges = edges;
         }
-    }
+    } 
+
+        
+       
+    
 }
